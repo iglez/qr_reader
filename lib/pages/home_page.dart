4b00d847937a2dc0fd45fefea96cc361b554873f
Ivan Gonzalez <ivan@iconos.mx>
2021-04-02 19:00:16 -0700

    App Bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,16 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Historial'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: () {}
+          ),
+        ],
+      ),
       body: Center(
         child: Text('Home page'),
       ),
