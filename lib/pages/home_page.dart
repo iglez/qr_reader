4d272524c4070d4cfdf1ec70f7d4c3ea67ec0191
Ivan Gonzalez <ivan@iconos.mx>
2021-04-02 19:04:36 -0700

    Creando Custom nativation bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/widgets/custom_navigatorbar.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -17,6 +18,7 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Text('Home page'),
       ),
+      bottomNavigationBar: CustomNavigationBar(),
     );
   }
 }
