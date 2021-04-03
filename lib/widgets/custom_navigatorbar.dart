3511b47c3ccbc9ec0bf9910a3bab3b4141e34988
Ivan Gonzalez <ivan@iconos.mx>
2021-04-02 22:01:02 -0700

    Leer del provider

--- lib/widgets/custom_navigatorbar.dart
+++ lib/widgets/custom_navigatorbar.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 
 class CustomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final currentIndex = 0;
+
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    final currentIndex = uiProvider.selectedMenuOpt;
 
     return BottomNavigationBar(
         elevation: 0,
