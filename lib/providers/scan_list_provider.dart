4da9d0ad4bc00a24968e8d1a0914865735ccc14a
Ivan Gonzalez <ivan@iconos.mx>
2021-04-04 22:12:59 -0700

    Nuevo Scan

--- lib/providers/scan_list_provider.dart
+++ lib/providers/scan_list_provider.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/models/scan_model.dart';
+import 'package:qr_reader/providers/db_provider.dart';
 
 class ScanListProvider extends ChangeNotifier {
+  List<ScanModel> scans = [];
+  String tipoSeleccionado = 'http';
 
+  nuevoScan(String valor) async {
+    ScanModel scanModel = ScanModel(valor: valor);
+    int id = await DBProvider.db.nuevoScan(scanModel);
+    scanModel.id = id;
+
+    this.scans.add(scanModel);
+    notifyListeners();
+  }
 }
