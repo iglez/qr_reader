7bc5ebf9b4f5fc7cee9a05b77f133b973e81f771
Ivan Gonzalez <ivan@iconos.mx>
2021-04-06 20:52:26 -0700

    Creando un List Tile para mostrar el cascaron que despues sera de base de datos.

--- lib/pages/mapas_page.dart
+++ lib/pages/mapas_page.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 class MapasPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Mapas page'),
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, i) => ListTile(
+        leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
+        title: Text('http://'),
+        subtitle: Text('ID'),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey,),
+        onTap: () => print('Abrir....'),
+      )
     );
   }
 }
