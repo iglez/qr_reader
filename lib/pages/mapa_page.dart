fb75300cc999b98efd6f8eecf3629232b62660c3
Ivan Gonzalez <ivan@iconos.mx>
2021-04-18 12:05:34 -0700

    convirtiendo statefull

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:qr_reader/models/scan_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
+  @override
+  _MapaPageState createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
   @override
   Widget build(BuildContext context) {
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
