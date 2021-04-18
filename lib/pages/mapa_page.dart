f1a1ede17ba68c44a58f36a2533f1a0d57f6b293
Ivan Gonzalez <ivan@iconos.mx>
2021-04-18 12:07:29 -0700

    cargando punto inciial

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:qr_reader/models/scan_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -8,8 +10,18 @@ class MapaPage extends StatefulWidget {
 }
 
 class _MapaPageState extends State<MapaPage> {
+
+  // https://pub.dev/packages/google_maps_flutter/versions/1.0.6
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
+
+    final CameraPosition puntoInicial = CameraPosition(
+        target: LatLng(37.42796133580664, -122.085749655962),
+        zoom: 14.4746,
+      );
+
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
