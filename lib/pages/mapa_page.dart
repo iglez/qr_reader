2169a360229710eab9e16c91b6a3a3d3a5f22c5d
Ivan Gonzalez <ivan@iconos.mx>
2021-04-18 12:27:24 -0700

    Regresando a la posicion inicial

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -10,33 +10,37 @@ class MapaPage extends StatefulWidget {
 }
 
 class _MapaPageState extends State<MapaPage> {
-
   // https://pub.dev/packages/google_maps_flutter/versions/1.0.6
   Completer<GoogleMapController> _controller = Completer();
 
   @override
   Widget build(BuildContext context) {
-
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
 
-    final CameraPosition _puntoInicial = CameraPosition(
-        target: scan.getLatLng(),
-        zoom: 17.5,
-        tilt: 50
-      );
+    final CameraPosition _puntoInicial =
+        CameraPosition(target: scan.getLatLng(), zoom: 17.5, tilt: 50);
 
     // marcadores
     Set<Marker> markers = new Set<Marker>();
-    markers.add(
-      Marker(
-        markerId: MarkerId('geo-location'),
-        position: scan.getLatLng(),
-      )
-    );
+    markers.add(Marker(
+      markerId: MarkerId('geo-location'),
+      position: scan.getLatLng(),
+    ));
 
     return Scaffold(
-      appBar: AppBar(title: Text('Mapa'),),
-      body:  GoogleMap(
+      appBar: AppBar(
+        title: Text('Mapa'),
+        actions: [
+          IconButton(icon: Icon(Icons.location_disabled_outlined), 
+          onPressed: () async {
+            // https://pub.dev/packages/google_maps_flutter/versions/1.0.6
+            final GoogleMapController controller = await _controller.future;
+            controller.animateCamera(CameraUpdate.newCameraPosition(_puntoInicial));
+          }
+          )
+        ],
+      ),
+      body: GoogleMap(
         myLocationButtonEnabled: false,
         mapType: MapType.normal,
         markers: markers,
